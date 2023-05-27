d8cf89380fd8d5b4ec2d40b4cf0bb6e72a120e57
Avhishek Adhikary <32614982+AvhishekAdhikary@users.noreply.github.com>
2023-05-27 23:28:56 +0530

    initial commit

--- src/Next.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Next = () => {
-  return <div />;
-};
-
-export default Next;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,19 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBox = ({ searchField, searchChange }) => {
   return (
-    <div>
-      <input type="search" placeholder="Search" onChange={searchChange} />
+    <div className="searchBox">
+      <input
+        className="searchInput"
+        type="search"
+        name=""
+        placeholder="Search"
+        onChange={searchChange}
+      />
+      <button className="searchButton" onClick={searchChange}>
+        <SearchIcon />
+      </button>
     </div>
   );
 };
